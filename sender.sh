c028925bfe7952c39d3009100587f9c7b87e1730
and189 <and1_89@live.de>
2024-03-19 19:34:17 +0000

    Update sender.sh

--- sender.sh
+++ sender.sh
@@ -8,12 +8,30 @@ DEVICE_NAME=$(sed -n 's/^.*<string name="origin">\(.*\)<\/string>.*$/\1/p' /data
 if [ -z "$DEVICE_NAME" ]; then
     DEVICE_NAME=$(hostname)
 fi
-# logcat-Output filtern und an Server senden
+
+# Tempor^dre Datei f^ar logcat-Output
+LOGCAT_FILE="/sdcard/logcat_$DEVICE_NAME.log"
+
+# Maximale Dateigr^t�e in Bytes (z.B. 1 MB)
+MAX_FILE_SIZE=$((1 * 1024 * 1024))
+
+# Log-Datei erstellen, falls sie nicht existiert
+touch $LOGCAT_FILE
+
+# Logcat in einer separaten Schleife in eine Datei schreiben
+(logcat | grep VM > $LOGCAT_FILE) &
+
+# Periodisch die letzten Zeilen der Logcat-Datei lesen und an den Server senden
 while true; do
-    logcat | grep VM | while read line; do
+    tail -n 300 $LOGCAT_FILE | while read line; do
         curl -X POST \
             -d "device_name=$DEVICE_NAME" \
             -d "log_data=$line" \
             $SERVER_URL
     done
-done
+    sleep 5  # Pausieren zwischen den Sendungen, um die Serverlast zu reduzieren
+
+    # Dateigr^t�e ^aberpr^afen und Log-Datei zur^acksetzen, falls sie zu gro� wird
+    if [ $(stat -c%s "$LOGCAT_FILE") -gt $MAX_FILE_SIZE ]; then
+        > $LOGCAT_FILE
+    fi
